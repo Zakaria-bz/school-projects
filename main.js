1e4012af80244626ee858c6be56188f4c019e864
اسمك <zakaria1201@proton.me>
2025-09-30 21:23:08 +0100

    That are my works

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ const texts = {
     conclusionTitle: "Conclusion :",
     conclusionText:
       "A computer is like a team of many members. Each member has a place in the team. When they work together, the system is complete. In simple words, a computer is not one thing. It is many things connected as one box, and that makes it powerful.",
-    designerBtn: "Zakaria Bezziou",
+    designerBtn: "Designer :",
     designerItems: [
       "Frontend Developer",
       "UI/UX Designer",
@@ -46,21 +46,21 @@ const texts = {
   },
   ar: {
     title: "ما هو الحاسوب ؟",
-    introTitle: "المقدمة :",
+    introTitle: " : المقدمة",
     introText: "الحاسوب آلة مهمة في حياتنا. نستخدمه للدراسة والعمل والترفيه.",
-    mainPartsTitle: "الأجزاء الرئيسية للحاسوب :",
+    mainPartsTitle: " : الأجزاء الرئيسية للحاسوب",
     mainPartsText:
       "للحاسوب جانبان كبيران: العتاد والبرمجيات. العتاد هو ما نراه ونلمسه، والبرمجيات هي البرامج التي تعمل على الجهاز.",
-    internalTitle: "المكوّنات الداخلية :",
+    internalTitle: ": المكونات الداخلية",
     internalText:
       "بداخل الحاسوب توجد العديد من الأجزاء. كلها متصلة على اللوحة الأم. بعض الأجزاء تساعد في المعالجة، أخرى للتخزين، وأخرى للطاقة. كل هذه المكونات تبقى داخل الصندوق.",
-    externalTitle: "المكوّنات الخارجية :",
+    externalTitle: " : المكونات الخارجية",
     externalText:
       "خارج الحاسوب نجد أيضًا العتاد. هذه الأدوات نستخدمها بأيدينا وأعيننا. تشمل الكتابة، التحريك، العرض، الطباعة، والصوت. هذه المكونات الخارجية تجعل الحاسوب سهل الاستخدام.",
-    togetherTitle: "كيف تعمل معًا :",
+    togetherTitle: " : كيف تعمل معا :",
     togetherText:
       "عند استخدامنا للحاسوب، تعمل جميع المكونات معًا. الأجزاء الداخلية تقوم بالعمل الأساسي. والأجزاء الخارجية تسمح لنا بالتحكم. إذا غاب جزء واحد، فلن يعمل الجهاز جيدًا.",
-    conclusionTitle: "الخاتمة :",
+    conclusionTitle: " : المكونات ",
     conclusionText:
       "الحاسوب مثل فريق من عدة أعضاء. لكل عضو دور في الفريق. عندما يعملون معًا، يكتمل النظام. ببساطة، الحاسوب ليس شيئًا واحدًا، بل عدة أشياء متصلة في صندوق واحد.",
     designerBtn: "زكرياء بزيو",
